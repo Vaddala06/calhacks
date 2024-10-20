7cfb48d2aaecfc32eb0b3305932d9c86f6ef64f4
Dhruv Shah <79233346+dpshah1@users.noreply.github.com>
2024-10-19 21:07:11 -0700

    Add files via upload

--- db_test.py
+++ db_test.py
@@ -1,32 +1,49 @@
 import chromadb
-chroma_client = chromadb.Client()
 
-path_collection = chroma_client.create_collection("path_collection")
-alert_collection = chroma_client.create_collection("alert_collection")
-
-path_collection.add(
-    documents=[
-        "This is a document about pineapple",
-        "This is a document about oranges",
-        "This is a document about apples",
-    ],
-
-    ids=['id_1', 'id_2', 'id_3']
+chroma_client = chromadb.PersistentClient(path="db.sqlite3", 
+    settings=chromadb.config.Settings(allow_reset=True)
 )
 
-alert_collection.add(
-    documents=[
-        "Alert: House is getting robbed",
-        "Alert: Your package has arrived",
-        "Alert: A black car passes by",
-    ],
+alert_collection = chroma_client.get_collection("alert_collection")
+#alert_collection = chroma_client.create_collection("alert_collection")
 
-    ids=['id_1', 'id_2', 'id_3']
-)
 
-results = alert_collection.query(
-    query_texts=["package"], # Chroma will embed this for you
-    n_results=1 # how many results to return
-)
 
-print(results)
+# alert_collection.add(
+#     documents=[
+#         "Alert: House is getting robbed",
+#         "Alert: Your package has arrived",
+#         "Alert: A black car passes by",
+#     ],
+
+#     metadatas=[
+#         {'path': "filepath1.mp4"},
+#         {'path': "filepath2.mp4"},
+#         {'path': "filepath3.mp4"},
+#     ],
+
+#     ids=['id_1', 'id_2', 'id_3']
+# )
+
+# results = alert_collection.query(
+#     query_texts=["danger"], # Chroma will embed this for you
+#     n_results=1, # how many results to return
+    
+# )
+
+# print(results)
+
+def add_motion_video(alert, file_path, id):
+    alert_collection.add(
+        documents=[alert],
+        metadatas=[{'path': file_path}],
+        ids=[f"id_{id}"] # make the id unique
+    )
+
+# chroma_client.reset()
+
+def get_all_alerts():
+
+    results = alert_collection.get()
+
+    return results
